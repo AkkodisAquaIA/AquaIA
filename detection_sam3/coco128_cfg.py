16995ce6269a3cef81c2ffae6bd74ba11119bc8c
Zhijian ZHOU <zhijian2040.zhou@gmail.com>
2026-01-15 17:51:47 +0100

    Config file

---
+++ detection_sam3/coco128_cfg.py
@@ -0,0 +1,7 @@
+IMAGES_FOLDER = "C:/Users/zhijian.zhou/OneDrive - Akkodis/Travail/10_AquaIA/08_Data/coco128/images/train2017"
+SAM3_CONF = 0.25
+SAM3_TASK = "segment"
+SAM3_MODE = "predict"
+SAM3_PATH = "C:/Users/zhijian.zhou/OneDrive - Akkodis/Travail/10_AquaIA/01_Git/sam3.pt"
+SAM3_HALF = True  # Use FP16 for faster inference
+SAM3_SAVE = True  # Save results to project folder
--- detection_sam3/coco128_detectionEntry.py
+++ detection_sam3/coco128_detectionEntry.py
@@ -2,14 +2,7 @@ from pathlib import Path
 from datetime import datetime
 from ultralytics.models.sam import SAM3SemanticPredictor
 from coco128_dict import COCO128_DICT
-
-IMAGES_FOLDER = "C:/Users/zhijian.zhou/OneDrive - Akkodis/Travail/10_AquaIA/08_Data/coco128/images/train2017"
-SAM3_CONF = 0.25
-SAM3_TASK = "segment"
-SAM3_MODE = "predict"
-SAM3_PATH = "C:/Users/zhijian.zhou/OneDrive - Akkodis/Travail/10_AquaIA/01_Git/sam3.pt"
-SAM3_HALF = True  # Use FP16 for faster inference
-SAM3_SAVE = True  # Save results to project folder
+from coco128_cfg import IMAGES_FOLDER, SAM3_CONF, SAM3_TASK, SAM3_MODE, SAM3_PATH, SAM3_HALF, SAM3_SAVE
 
 # Initialize SAM3 predictor with configuration
 current_folder = Path(__file__).resolve().parent
