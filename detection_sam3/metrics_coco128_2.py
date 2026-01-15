d14bf79193f2e66085e4aeedf0b4f6531c13bb48
Zhijian ZHOU <zhijian2040.zhou@gmail.com>
2026-01-15 16:11:34 +0100

    Result saving folder

--- detection_sam3/metrics_coco128_2.py
+++ detection_sam3/metrics_coco128_2.py
@@ -1,12 +1,13 @@
 from pathlib import Path
-
+from datetime import datetime
 from ultralytics.models.sam import SAM3SemanticPredictor
-
 from coco128_dict import COCO128_DICT
 
 MODEL_PATH = "C:/Users/zhijian.zhou/OneDrive - Akkodis/Travail/10_AquaIA/01_Git/sam3.pt"
 IMAGES_FOLDER = "C:/Users/zhijian.zhou/OneDrive - Akkodis/Travail/10_AquaIA/08_Data/coco128/images/train2017"
-SAVE_FOLDER = "C:/Users/zhijian.zhou/OneDrive - Akkodis/Travail/10_AquaIA/01_Git/detection_sam3/metrics_coco128_2_results"
+
+current_dir = Path(__file__).resolve().parent  # Directory of the current script
+timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
 
 # Initialize predictor with configuration
 overrides = dict(
@@ -16,14 +17,15 @@ overrides = dict(
     model=MODEL_PATH,
     half=True,  # Use FP16 for faster inference
     save=True,
-)
+    project=str(current_dir),
+    name=f"result_det_{timestamp}",)
 predictor = SAM3SemanticPredictor(overrides=overrides)
 
 # Text prompts sourced from the COCO label dictionary (keys sorted for stable order)
 text_prompts = [COCO128_DICT[idx] for idx in sorted(COCO128_DICT.keys())]
 
 # Iterate over all images in IMAGES_FOLDER and run inference with the text prompts
-image_files = sorted(f for f in Path(IMAGES_FOLDER).glob("**/*") if f.suffix.lower() in {".jpg", ".jpeg", ".png"})
+image_files = sorted(f for f in Path(IMAGES_FOLDER).glob("**/*") if f.suffix.lower() in {".jpg", ".jpeg", ".png"})[0:2]
 
 for img in image_files:
     predictor.set_image(str(img))
