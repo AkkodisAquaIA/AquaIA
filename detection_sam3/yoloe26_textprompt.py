542b32bbc1006e39537492f4b9a4f3ca332f0879
Zhijian ZHOU <zhijian2040.zhou@gmail.com>
2026-02-01 20:01:31 +0100

    yolo26 with parameters

--- detection_sam3/yoloe26_textprompt.py
+++ detection_sam3/yoloe26_textprompt.py
@@ -1,14 +1,27 @@
+from pathlib import Path
+from datetime import datetime
 from ultralytics import YOLOE
 
+# Initialize SAM3 predictor with configuration
+current_folder = Path(__file__).resolve().parent
+timestamp = datetime.now().strftime("%Y%m%d%H%M")
+
 # Initialize a YOLOE model
-model = YOLOE("C:/Users/zhijian.zhou/OneDrive - Akkodis/Travail/10_AquaIA/01_Git/yoloe-26x-seg.pt")  # or yoloe-26s/m-seg.pt for different sizes
+model = YOLOE("C:/Users/zhijian.zhou/OneDrive - Akkodis/Travail/10_AquaIA/01_Git/yoloe-26x-seg.pt")
 
 # Set text prompt to detect person and bus. You only need to do this once after you load the model.
 names = ["person", "bus"]
 model.set_classes(names, model.get_text_pe(names))
 
 # Run detection on the given image
-results = model.predict("C:/Users/zhijian.zhou/OneDrive - Akkodis/Bureau/unnamed.jpg")
+results = model.predict(
+    source="C:/Users/zhijian.zhou/OneDrive - Akkodis/Bureau/unnamed.jpg",
+    conf=0.25,   # confidence
+    half=True,   # FP16
+    save=True,   # 保存结果
+    imgsz=320,   # 输入尺寸
+    project=str(current_folder),
+    name=f"yolo_result_det_{timestamp}",)
 
 # Show results
 results[0].show()
