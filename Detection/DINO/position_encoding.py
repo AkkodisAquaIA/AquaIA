49a1681c355365319650723ac37a05deedacd19f
BEAUSSANT Samuel <Samuel.BEAUSSANT@akkodis.com>
2026-02-10 09:37:34 +0100

    cleaning up some unused files

--- Detection/DINO/position_encoding.py
+++ Detection/DINO/position_encoding.py
@@ -85,43 +85,6 @@ class PositionEmbeddingSine(nn.Module):
         return pos
 
 
-# class PositionEmbeddingLearned(nn.Module):
-#     """
-#     Absolute pos embedding, learned.
-#     """
-
-#     def __init__(self, num_pos_feats=256):
-#         super().__init__()
-#         self.row_embed = nn.Embedding(50, num_pos_feats)
-#         self.col_embed = nn.Embedding(50, num_pos_feats)
-#         self.reset_parameters()
-
-#     def reset_parameters(self):
-#         nn.init.uniform_(self.row_embed.weight)
-#         nn.init.uniform_(self.col_embed.weight)
-
-#     def forward(self, tensor_list: NestedTensor):
-#         x = tensor_list.tensors
-#         h, w = x.shape[-2:]
-#         i = torch.arange(w, device=x.device)
-#         j = torch.arange(h, device=x.device)
-#         x_emb = self.col_embed(i)
-#         y_emb = self.row_embed(j)
-#         pos = (
-#             torch.cat(
-#                 [
-#                     x_emb.unsqueeze(0).repeat(h, 1, 1),
-#                     y_emb.unsqueeze(1).repeat(1, w, 1),
-#                 ],
-#                 dim=-1,
-#             )
-#             .permute(2, 0, 1)
-#             .unsqueeze(0)
-#             .repeat(x.shape[0], 1, 1, 1)
-#         )
-#         return pos
-
-
 def build_position_encoding(pe_type, h_dim=256):
     N_steps = h_dim // 2
     if pe_type == "sine":  # also called v2
